aea9da20948e347a8f2f8b5671b9bbc1d8d3cadc
sheeshpal84 <sheeshpalkotwal@gmail.com>
2022-01-03 21:59:31 +0530

    about-section done

--- inc/about.php
+++ inc/about.php
@@ -1,17 +1,42 @@
 <div class="container-fluid about">
-    <div class="row py-padding">
-        <div class="col-12 text-center centent-about">
-            <div>
-                <h5>Adventure Yearly Roundtrip</h5>
-                <h2>About Us</h2>
-                <img src="images/icon-text.png" alt="TEXT" class="img-fluid">
-            </div>
-        </div>
-        <div class="col-12 col-md-6">
-          <div>sk </div>
-          <div> <h2>Our Favorite <span> Adventure Trips</span> of 2021</h2></div>
-        </div>
-        <div class="col-12 col-md-6"></div>
+  <div class="row py-padding">
+    <div class="col-12 text-center centent-about pb-5">
+      <div>
+        <h5>Adventure Yearly Roundtrip</h5>
+        <h2>About Us</h2>
+        <img src="images/icon-text.png" alt="TEXT" class="img-fluid">
+      </div>
     </div>
+    <div class="col-12 col-md-6 about-centent">
+      <div>
+        <h2>Our Favorite <span> Adventure Trips</span> of 2021</h2>
+        <p>Lorem Ipsum is simply dummy text of the printing and pesetting . simply Lorem Ipsum has been the industry's since the 1500s, Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+        <button type="button" class="btn btn-primary btn-lg">SEE MORE</button>
+      </div>
+      <div class="row d-flex  align-items-end">
+        <div class="col-12 col-md-6"> <img src="images/about-img-2.png" alt="about-1" class="img-fluid"></div>
+        <div class="col-12 col-md-6"> <img src="images/about-img-1.png" alt="about-2" class="img-fluid"></div>
+      </div>
+    </div>
+    <div class="col-12 col-md-6 about-slider">
+      <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="images\about-slider.png" class="d-block w-100 img-fluid" alt="about-slider-1">
+          </div>
+          <div class="carousel-item">
+            <img src="images\about-slider.png" class="d-block w-100 img-fluid" alt="about-slider-2">
+          </div>
+          <div class="carousel-item">
+            <img src="images\about-slider.png" class="d-block w-100 img-fluid" alt="about-slider-2">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
-
